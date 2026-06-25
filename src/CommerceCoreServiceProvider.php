d3ffde01b2dc05665ed5b56055b3eea40d354c94
James Liang <lalalili@gmail.com>
2026-06-25 20:07:25 +0800

    chore: 補齊 commerce-core singleton 註冊

--- src/CommerceCoreServiceProvider.php
+++ src/CommerceCoreServiceProvider.php
@@ -2,17 +2,32 @@
 
 namespace Lalalili\CommerceCore;
 
+use Lalalili\CommerceCore\Services\CartItemAttributeNormalizer;
+use Lalalili\CommerceCore\Services\CartPromotionLineResolver;
+use Lalalili\CommerceCore\Services\CheckoutCartCompletionService;
+use Lalalili\CommerceCore\Services\CheckoutOrderBuilderService;
+use Lalalili\CommerceCore\Services\CheckoutOrderDataFactory;
 use Lalalili\CommerceCore\Services\CheckoutService;
 use Lalalili\CommerceCore\Services\CheckoutSnapshotService;
+use Lalalili\CommerceCore\Services\CouponCheckoutAdapterService;
 use Lalalili\CommerceCore\Services\CouponCodeGenerationPolicy;
+use Lalalili\CommerceCore\Services\CouponDataPayloadResolver;
 use Lalalili\CommerceCore\Services\CouponEligibilityCartDataFactory;
+use Lalalili\CommerceCore\Services\CouponInventoryService;
+use Lalalili\CommerceCore\Services\CouponPricingTraceEntryFactory;
+use Lalalili\CommerceCore\Services\CouponPricingTraceService;
+use Lalalili\CommerceCore\Services\CouponTracePayloadResolver;
 use Lalalili\CommerceCore\Services\EntitlementService;
+use Lalalili\CommerceCore\Services\InvoiceIssueSchedulerService;
+use Lalalili\CommerceCore\Services\OrderDetailAdjustmentService;
 use Lalalili\CommerceCore\Services\OrderInitialStatusResolver;
+use Lalalili\CommerceCore\Services\OrderInvoiceTaxGroupService;
 use Lalalili\CommerceCore\Services\OrderLifecycleHookDispatcher;
 use Lalalili\CommerceCore\Services\OrderLifecycleService;
 use Lalalili\CommerceCore\Services\PaymentApplicationHookDispatcher;
 use Lalalili\CommerceCore\Services\PaymentApplicationService;
 use Lalalili\CommerceCore\Services\PaymentLogService;
+use Lalalili\CommerceCore\Services\PromotionRefreshPipelineMetadata;
 use Lalalili\CommerceCore\Support\ModelAttributeMapper;
 use Lalalili\CommerceCore\Support\OrderItemNormalizer;
 use Lalalili\CommerceCore\Support\OrderNumberGenerator;
@@ -36,16 +51,31 @@ class CommerceCoreServiceProvider extends PackageServiceProvider
         $this->app->singleton(ModelAttributeMapper::class);
         $this->app->singleton(OrderItemNormalizer::class);
         $this->app->singleton(OrderNumberGenerator::class);
+        $this->app->singleton(CartItemAttributeNormalizer::class);
+        $this->app->singleton(CartPromotionLineResolver::class);
+        $this->app->singleton(CheckoutCartCompletionService::class);
+        $this->app->singleton(CheckoutOrderBuilderService::class);
+        $this->app->singleton(CheckoutOrderDataFactory::class);
         $this->app->singleton(CheckoutService::class);
         $this->app->singleton(CheckoutSnapshotService::class);
+        $this->app->singleton(CouponCheckoutAdapterService::class);
         $this->app->singleton(CouponCodeGenerationPolicy::class);
+        $this->app->singleton(CouponDataPayloadResolver::class);
         $this->app->singleton(CouponEligibilityCartDataFactory::class);
+        $this->app->singleton(CouponInventoryService::class);
+        $this->app->singleton(CouponPricingTraceEntryFactory::class);
+        $this->app->singleton(CouponPricingTraceService::class);
+        $this->app->singleton(CouponTracePayloadResolver::class);
         $this->app->singleton(EntitlementService::class);
+        $this->app->singleton(InvoiceIssueSchedulerService::class);
+        $this->app->singleton(OrderDetailAdjustmentService::class);
         $this->app->singleton(OrderInitialStatusResolver::class);
+        $this->app->singleton(OrderInvoiceTaxGroupService::class);
         $this->app->singleton(OrderLifecycleHookDispatcher::class);
         $this->app->singleton(OrderLifecycleService::class);
         $this->app->singleton(PaymentApplicationHookDispatcher::class);
         $this->app->singleton(PaymentApplicationService::class);
         $this->app->singleton(PaymentLogService::class);
+        $this->app->singleton(PromotionRefreshPipelineMetadata::class);
     }
 }
---
+++ tests/Feature/CommerceCoreServiceProviderTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use Lalalili\CommerceCore\Services\CartItemAttributeNormalizer;
+use Lalalili\CommerceCore\Services\CartPromotionLineResolver;
+use Lalalili\CommerceCore\Services\CheckoutCartCompletionService;
+use Lalalili\CommerceCore\Services\CheckoutOrderBuilderService;
+use Lalalili\CommerceCore\Services\CheckoutOrderDataFactory;
+use Lalalili\CommerceCore\Services\CheckoutService;
+use Lalalili\CommerceCore\Services\CheckoutSnapshotService;
+use Lalalili\CommerceCore\Services\CouponCheckoutAdapterService;
+use Lalalili\CommerceCore\Services\CouponCodeGenerationPolicy;
+use Lalalili\CommerceCore\Services\CouponDataPayloadResolver;
+use Lalalili\CommerceCore\Services\CouponEligibilityCartDataFactory;
+use Lalalili\CommerceCore\Services\CouponInventoryService;
+use Lalalili\CommerceCore\Services\CouponPricingTraceEntryFactory;
+use Lalalili\CommerceCore\Services\CouponPricingTraceService;
+use Lalalili\CommerceCore\Services\CouponTracePayloadResolver;
+use Lalalili\CommerceCore\Services\EntitlementService;
+use Lalalili\CommerceCore\Services\InvoiceIssueSchedulerService;
+use Lalalili\CommerceCore\Services\OrderDetailAdjustmentService;
+use Lalalili\CommerceCore\Services\OrderInitialStatusResolver;
+use Lalalili\CommerceCore\Services\OrderInvoiceTaxGroupService;
+use Lalalili\CommerceCore\Services\OrderLifecycleHookDispatcher;
+use Lalalili\CommerceCore\Services\OrderLifecycleService;
+use Lalalili\CommerceCore\Services\PaymentApplicationHookDispatcher;
+use Lalalili\CommerceCore\Services\PaymentApplicationService;
+use Lalalili\CommerceCore\Services\PaymentLogService;
+use Lalalili\CommerceCore\Services\PromotionRefreshPipelineMetadata;
+use Lalalili\CommerceCore\Support\ModelAttributeMapper;
+use Lalalili\CommerceCore\Support\OrderItemNormalizer;
+use Lalalili\CommerceCore\Support\OrderNumberGenerator;
+
+it('registers reusable commerce services as singletons', function (string $abstract): void {
+    expect(app()->isShared($abstract))->toBeTrue();
+})->with([
+    ModelAttributeMapper::class,
+    OrderItemNormalizer::class,
+    OrderNumberGenerator::class,
+    CartItemAttributeNormalizer::class,
+    CartPromotionLineResolver::class,
+    CheckoutCartCompletionService::class,
+    CheckoutOrderBuilderService::class,
+    CheckoutOrderDataFactory::class,
+    CheckoutService::class,
+    CheckoutSnapshotService::class,
+    CouponCheckoutAdapterService::class,
+    CouponCodeGenerationPolicy::class,
+    CouponDataPayloadResolver::class,
+    CouponEligibilityCartDataFactory::class,
+    CouponInventoryService::class,
+    CouponPricingTraceEntryFactory::class,
+    CouponPricingTraceService::class,
+    CouponTracePayloadResolver::class,
+    EntitlementService::class,
+    InvoiceIssueSchedulerService::class,
+    OrderDetailAdjustmentService::class,
+    OrderInitialStatusResolver::class,
+    OrderInvoiceTaxGroupService::class,
+    OrderLifecycleHookDispatcher::class,
+    OrderLifecycleService::class,
+    PaymentApplicationHookDispatcher::class,
+    PaymentApplicationService::class,
+    PaymentLogService::class,
+    PromotionRefreshPipelineMetadata::class,
+]);
